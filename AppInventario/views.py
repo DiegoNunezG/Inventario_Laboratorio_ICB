061fe9a1b95b470187ef5d29a36c2278675c5d75
RenataArcos <rarcos21@alumnos.utalca.cl>
2024-06-06 23:50:47 -0400

    primera versión del sidebar

--- AppInventario/views.py
+++ AppInventario/views.py
@@ -3,6 +3,3 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "AppInventario/base.html")
-
-def tipo_de_producto(request):
-    return render(request, 'AppInventario/tipo_de_producto.html')
--- Gestion_Inventario/urls.py
+++ Gestion_Inventario/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from AppInventario.views import index, tipo_de_producto
+from AppInventario.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
-    path('tipo-de-producto/', tipo_de_producto, name="tipo_de_producto"),
 ]
