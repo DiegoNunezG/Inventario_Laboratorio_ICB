93b4aa7e1c6dd29bf5eae832c503d274f9aab219
RenataArcos <rarcos21@alumnos.utalca.cl>
2024-06-06 22:06:25 -0400

    añadí página de tipo de producto

--- AppInventario/views.py
+++ AppInventario/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "AppInventario/base.html")
+
+def tipo_de_producto(request):
+    return render(request, 'AppInventario/tipo_de_producto.html')
--- Gestion_Inventario/urls.py
+++ Gestion_Inventario/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from AppInventario.views import index
+from AppInventario.views import index, tipo_de_producto
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('tipo-de-producto/', tipo_de_producto, name="tipo_de_producto"),
 ]
