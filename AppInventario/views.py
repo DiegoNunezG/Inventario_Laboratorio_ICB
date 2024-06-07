a21d2461f57677b9bfb98a7fe418b02e5e4783f5
RenataArcos <rarcos21@alumnos.utalca.cl>
2024-06-07 10:25:33 -0400

    Revert "Merge pull request #38 from DiegoNunezG/Actualizar_logout"
    This reverts commit 5da566317272b613b1feca8a48c19b3bdaa37515, reversing
    changes made to 853539f39e45e625e8dad2f44dc02ded7a74476c.

--- AppInventario/views.py
+++ AppInventario/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from .models import TipoProducto
 
-
 def login_web(request):
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
--- Gestion_Inventario/urls.py
+++ Gestion_Inventario/urls.py
@@ -6,7 +6,7 @@ from AppInventario.views import login_web, index, tipo_de_producto
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', login_web, name="login"),
-    path('', LogoutView.as_view(), name='logout'), #aqui se puede cambiar el path de logout
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('index/', index),
     path('tipo-de-producto/', tipo_de_producto, name='tipo_de_producto'),
 ]
